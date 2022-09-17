9c0b23c9ca7a269829f313d19493295c684c33a7
Gabriel Mazurco <mazurco066@gmail.com>
2022-09-17 01:20:08 -0300

    chore(ideas): planning new home page

--- src/presentation/ui/views/home/index.tsx
+++ src/presentation/ui/views/home/index.tsx
@@ -3,18 +3,31 @@ import { FC } from 'react'
 import { useUser } from 'infra/services/session'
 
 // Layout and Components
-import { Container } from '@chakra-ui/react'
+import {
+  Container,
+  Heading,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 // Sign in component
 const HomeView: FC = () => {
   // Hooks
   const { user } = useUser()
 
+  // Color hooks
+  const showSubtitleColor = useColorModeValue('gray.500', 'gray.400')
+
   // View JSX
   return (
     <div>
       <Container>
-     
+        <Heading as="h3" size="xl">
+          Bem vindo(a)!
+        </Heading>
+        <Text color={showSubtitleColor}>
+          3 Apresentações pendentes
+        </Text>
       </Container>
     </div>
   )
