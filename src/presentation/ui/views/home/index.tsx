64f69df13ab3fd3a03d2d940793b03993b0423a6
Gabriel Mazurco <mazurco066@gmail.com>
2023-05-21 14:36:11 -0300

    build(1.2.3): build for 1.2.3

--- src/presentation/ui/views/home/index.tsx
+++ src/presentation/ui/views/home/index.tsx
@@ -192,7 +192,7 @@ const HomeView: FC = () => {
         <Text
           textAlign="center"
         >
-          {t('version')}<Text as="strong" color="secondary.500">1.2.2</Text>
+          {t('version')}<Text as="strong" color="secondary.500">1.2.3</Text>
         </Text>
       </Container>
     </div>
