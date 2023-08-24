32c8c8985534036d96a6b092bb4607b80f8e21e8
Gabriel Mazurco <mazurco066@gmail.com>
2023-08-23 23:06:30 -0300

    chore(scrap song): disable import song feature

--- src/presentation/ui/views/saveSong/index.tsx
+++ src/presentation/ui/views/saveSong/index.tsx
@@ -358,6 +358,8 @@ const SaveSongView: FC<{
           borderRadius="lg"
           px="3"
           py="3"
+          // Feature disabled because of server hardware limitation
+          display="none"
         >
           <Text
             color="gray.100"
