bd8aa00cde440294b98b739bde7384660aec6412
Gabriel Mazurco <mazurco066@gmail.com>
2023-12-01 15:43:02 -0300

    feat(liturgy colors): added liturgy colors variations to pdf preview

--- src/presentation/utils/index.ts
+++ src/presentation/utils/index.ts
@@ -4,6 +4,7 @@ export * from './asyncRequestHandler'
 export * from './chordsData'
 export * from './chordTransposer'
 export * from './formatDate'
+export * from './getColorSchema'
 export * from './getUniques'
 export * from './tuncateStr'
 
