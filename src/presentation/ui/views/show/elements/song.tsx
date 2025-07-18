d08036a6f6991e48efd2af03a8d49630d4a13916
Gabriel Mazurco <mazurco066@gmail.com>
2025-07-18 13:20:49 -0300

    fix(build): fixed build script

--- src/presentation/ui/views/show/elements/song.tsx
+++ src/presentation/ui/views/show/elements/song.tsx
@@ -8,7 +8,7 @@ import type { SongType } from 'domain/models'
 // Components
 import { FaEllipsisV, FaHandRock, FaPlus } from 'react-icons/fa'
 import { DeleteIcon, EditIcon, Icon, ViewIcon } from '@chakra-ui/icons'
-import { Draggable, DraggableProvided } from 'react-beautiful-dnd'
+import { CSS } from '@dnd-kit/utilities'
 import {
   Box,
   Flex,
@@ -21,70 +21,75 @@ import {
   Text,
   useColorModeValue
 } from '@chakra-ui/react'
+import { useSortable } from '@dnd-kit/sortable'
 
 // Drag Component
 export const DraggableSongItem: FC<{ item: SongType, index: number }> = ({ item, index }) => {
   // Color Hooks
   const bgBox = useColorModeValue('blackAlpha.500', 'blackAlpha.500')
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: item.id });
+  
+  // Styles
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
 
   // Destruct song data
   const { id, title, writter } = item
 
   // JSX
   return (
-    <Draggable draggableId={id} index={index}>
-      {
-        (provided: DraggableProvided) => (
-          <Box
-            data-group
-            ref={provided.innerRef}
-            bgColor={bgBox}
-            width="full"
-            borderRadius="lg"
-            overflow="hidden"
-            transition="all 0.3s"
-            { ...provided.draggableProps }
+    <Box
+      data-group
+      bgColor={bgBox}
+      width="full"
+      borderRadius="lg"
+      overflow="hidden"
+      transition="all 0.3s"
+      cursor="grab"
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      <Flex
+        alignItems="center"
+        justifyContent="flex-start"
+        height="full"
+      >
+        <Box
+          width="48px"
+          height="64px"
+          flex="0 0 auto"
+        >
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            height="full"
           >
-            <Flex
-              alignItems="center"
-              justifyContent="flex-start"
-              height="full"
-            >
-              <Box
-                width="48px"
-                height="64px"
-                flex="0 0 auto"
-                { ...provided.dragHandleProps }
-              >
-                <Flex
-                  justifyContent="center"
-                  alignItems="center"
-                  height="full"
-                >
-                  <Icon as={FaHandRock} />
-                </Flex>
-              </Box>
-              <Box
-                flexGrow="1"
-                pr="2"
-                py="3"
-                pl="1"
-              >
-                <Heading
-                  as="h5"
-                  size="sm"
-                >
-                  {title}
-                </Heading>
-                <Text>
-                  {writter}
-                </Text>
-              </Box>
-            </Flex>
-          </Box>
-        )
-      }
-    </Draggable>
+            <Icon as={FaHandRock} />
+          </Flex>
+        </Box>
+        <Box
+          flexGrow="1"
+          pr="2"
+          py="3"
+          pl="1"
+        >
+          <Heading
+            as="h5"
+            size="sm"
+          >
+            {title}
+          </Heading>
+          <Text>
+            {writter}
+          </Text>
+        </Box>
+      </Flex>
+    </Box>
+
   )
 }
 
@@ -97,90 +102,90 @@ export const OrderedSong: FC<{
   order?: number,
   song: SongType
 }> = ({
-  onClick = () => {},
-  onEdit = () => {},
-  onRemove = () => {},
+  onClick = () => { },
+  onEdit = () => { },
+  onRemove = () => { },
   isLoading = false,
   order = 0,
   song
 }) => {
-  // Hooks
-  const { t } = useTranslation('concert')
+    // Hooks
+    const { t } = useTranslation('concert')
 
-  // Color Hooks
-  const bgBox = useColorModeValue('gray.50', 'gray.800')
-  const colorOrder = useColorModeValue('secondary.700', 'secondary.400')
+    // Color Hooks
+    const bgBox = useColorModeValue('gray.50', 'gray.800')
+    const colorOrder = useColorModeValue('secondary.700', 'secondary.400')
 
-  // Destruct song data
-  const { title, writter } = song
+    // Destruct song data
+    const { title, writter } = song
 
-  // JSX
-  return (
-    <Box
-      data-group
-      bgColor={bgBox}
-      width="full"
-      borderRadius="lg"
-      transition="all 0.3s"
-      _hover={{
-        cursor: 'pointer'
-      }}
-    >
-      <Flex
-        alignItems="center"
-        justifyContent="flex-start"
-        height="full"
+    // JSX
+    return (
+      <Box
+        data-group
+        bgColor={bgBox}
+        width="full"
+        borderRadius="lg"
+        transition="all 0.3s"
+        _hover={{
+          cursor: 'pointer'
+        }}
       >
-        {/* Order Section */}
-        <Box
-          width="48px"
+        <Flex
+          alignItems="center"
+          justifyContent="flex-start"
           height="full"
-          flex="0 0 auto"
         >
-          <Flex
-            justifyContent="center"
-            alignItems="center"
+          {/* Order Section */}
+          <Box
+            width="48px"
             height="full"
+            flex="0 0 auto"
           >
-            <Text
-              color={colorOrder}
-              fontWeight="semibold"
+            <Flex
+              justifyContent="center"
+              alignItems="center"
+              height="full"
             >
-              {order}
-            </Text>
-          </Flex>
-        </Box>
-        {/* Song data */}
-        <Box
-          flexGrow="1"
-          px="2"
-          py="3"
-        >
-          <Heading
-            as="h5"
-            size="sm"
-            _groupHover={{
-              color: colorOrder
-            }}
+              <Text
+                color={colorOrder}
+                fontWeight="semibold"
+              >
+                {order}
+              </Text>
+            </Flex>
+          </Box>
+          {/* Song data */}
+          <Box
+            flexGrow="1"
+            px="2"
+            py="3"
           >
-            {title}
-          </Heading>
-          <Text>
-            {writter}
-          </Text>
-        </Box>
-        <Box
-          flex="0 0 auto"
-          pr="3"
-        >
-          <Menu>
-            <MenuButton
-              as={IconButton}
-              aria-label='Options'
-              icon={<Icon as={FaEllipsisV} />}
-              variant='ghost'
-            />
-              <MenuList>   
+            <Heading
+              as="h5"
+              size="sm"
+              _groupHover={{
+                color: colorOrder
+              }}
+            >
+              {title}
+            </Heading>
+            <Text>
+              {writter}
+            </Text>
+          </Box>
+          <Box
+            flex="0 0 auto"
+            pr="3"
+          >
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label='Options'
+                icon={<Icon as={FaEllipsisV} />}
+                variant='ghost'
+              />
+              <MenuList>
                 <MenuItem
                   disabled={isLoading}
                   icon={<ViewIcon />}
@@ -201,14 +206,14 @@ export const OrderedSong: FC<{
                   onClick={onRemove}
                 >
                   {t('options.remove')}
-               </MenuItem>
-            </MenuList>
-          </Menu>
-        </Box>
-      </Flex>
-    </Box>
-  )
-}
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Flex>
+      </Box>
+    )
+  }
 
 // Add Component
 export const AddSong: FC<{
@@ -218,60 +223,60 @@ export const AddSong: FC<{
 }> = ({
   song: { title, writter },
   isLoading = false,
-  onClick = () => {}
+  onClick = () => { }
 }) => {
-  // JSX
-  return (
-    <Box
-      data-group
-      bgColor="blackAlpha.500"
-      width="full"
-      borderRadius="lg"
-      transition="all 0.3s"
-      _hover={{
-        cursor: 'pointer'
-      }}
-    >
-      <Flex
-        alignItems="center"
-        justifyContent="flex-start"
-        height="full"
+    // JSX
+    return (
+      <Box
+        data-group
+        bgColor="blackAlpha.500"
+        width="full"
+        borderRadius="lg"
+        transition="all 0.3s"
+        _hover={{
+          cursor: 'pointer'
+        }}
       >
-        <Box
-          flexGrow="1"
-          px="2"
-          py="3"
+        <Flex
+          alignItems="center"
+          justifyContent="flex-start"
+          height="full"
         >
-          <Heading
-            as="h5"
-            size="sm"
-            _groupHover={{
-              color: 'secondary.400'
-            }}
+          <Box
+            flexGrow="1"
+            px="2"
+            py="3"
           >
-            {title}
-          </Heading>
-          <Text>
-            {writter}
-          </Text>
-        </Box>
-        <Box
-          flex="0 0 auto"
-          pr="3"
-        >
-          <IconButton
-            disabled={isLoading}
-            aria-label="Add song"
-            backgroundColor="whiteAlpha.200"
-            _hover={{
-              backgroundColor: "whiteAlpha.300"
-            }}
-            icon={<Icon as={FaPlus} />}
+            <Heading
+              as="h5"
+              size="sm"
+              _groupHover={{
+                color: 'secondary.400'
+              }}
+            >
+              {title}
+            </Heading>
+            <Text>
+              {writter}
+            </Text>
+          </Box>
+          <Box
             flex="0 0 auto"
-            onClick={isLoading ? () => {} : () => onClick()}
-          />
-        </Box>
-      </Flex>
-    </Box>
-  )
-}
+            pr="3"
+          >
+            <IconButton
+              disabled={isLoading}
+              aria-label="Add song"
+              backgroundColor="whiteAlpha.200"
+              _hover={{
+                backgroundColor: "whiteAlpha.300"
+              }}
+              icon={<Icon as={FaPlus} />}
+              flex="0 0 auto"
+              onClick={isLoading ? () => { } : () => onClick()}
+            />
+          </Box>
+        </Flex>
+      </Box>
+    )
+  }
